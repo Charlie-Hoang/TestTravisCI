f4d6760315ddbfb16ed897cad39675e6acd1552c
Charlie <chickchick.ce@gmail.com>
2019-01-18 15:09:42 +0700

    remove error

--- TestTravisCI/ViewController.swift
+++ TestTravisCI/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        test error code a
         // Do any additional setup after loading the view, typically from a nib.
     }
 
